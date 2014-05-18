30d3c6a92def91d7e9d0c00a02ea5d3c88c43b1c
Ralf S. Engelschall <rse@engelschall.com>
2014-05-18 11:00:47 +0200

    align all MPL license headers

--- tst/core/binding-1-api.js
+++ tst/core/binding-1-api.js
@@ -3,8 +3,8 @@
 **  Copyright (c) 2014 Ralf S. Engelschall <http://engelschall.com>
 **
 **  This Source Code Form is subject to the terms of the Mozilla Public
-**  License, v. 2.0. If a copy of the MPL was not distributed with this
-**  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+**  License (MPL), version 2.0. If a copy of the MPL was not distributed
+**  with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 describe("BindingJS API Management", function () {
