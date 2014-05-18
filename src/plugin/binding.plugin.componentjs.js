35a69cb491b4f2979ce333e67429761a12586efe
Ralf S. Engelschall <rse@engelschall.com>
2014-05-18 23:46:22 +0200

    add a dummy plugin

---
+++ src/plugin/binding.plugin.componentjs.js
@@ -0,0 +1,15 @@
+/*
+**  BindingJS -- View Data Binding for JavaScript <http://bindingjs.com>
+**  Copyright (c) 2014 Ralf S. Engelschall <http://engelschall.com>
+**
+**  This Source Code Form is subject to the terms of the Mozilla Public
+**  License (MPL), version 2.0. If a copy of the MPL was not distributed
+**  with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+include("../umd/umd-plugins-prolog.js", { library: "bindingjs", plugin: "componentjs", deps: "" });
+
+//  $api.registerAdapter("FIXME");
+
+include("../umd/umd-plugins-epilog.js");
+
