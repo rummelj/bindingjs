9d796c48805fc5c7dffc20cfa0e57f266cd5ef14
Ralf S. Engelschall <rse@engelschall.com>
2014-06-25 15:39:25 +0200

    cleanup model

--- smp/foo-model.js
+++ smp/foo-model.js
@@ -1,12 +1,11 @@
-var model = {
+{
     "data:persons": [
-        { id: 1, name: "John", enabled: true, "event:kill": false, },
-        { id: 2, name: "Jim", enabled: false, "event:kill": false, },
-        { id: 3, name: "Bob", enabled: true, "event:kill": false, }
+        { id: 1, name: "John", enabled: true,  "event:kill": false },
+        { id: 2, name: "Jim",  enabled: false, "event:kill": false },
+        { id: 3, name: "Bob",  enabled: true,  "event:kill": false }
     ],
-    "param:window.begin": 0,
-    "param:window.end": 3,
     "data:intro": "lorem ipsum",
+    "param:window": { "begin": 0, "end": 3 },
     "param:lang": "en",
     "event:kill-persons": false,
     "event:new-persons": false
