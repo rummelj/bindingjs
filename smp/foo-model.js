e94878ca433372fb43ab3382dda6b154f374bbfa
Johannes Rummel <johannesrummel@googlemail.com>
2014-06-25 10:46:49 +0200

    Added model to smp

---
+++ smp/foo-model.js
@@ -0,0 +1,13 @@
+var model = {
+    "data:persons": [
+        { id: 1, name: "John", enabled: true, "event:kill": false, },
+        { id: 2, name: "Jim", enabled: false, "event:kill": false, },
+        { id: 3, name: "Bob", enabled: true, "event:kill": false, }
+    ],
+    "param:window.begin": 0,
+    "param:window.end": 3,
+    "data:intro": "lorem ipsum",
+    "param:lang": "en",
+    "event:kill-persons": false,
+    "event:new-persons": false
+}
