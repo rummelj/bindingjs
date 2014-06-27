c385b4dfd9ae04832cb32fd4a21ab7d7b59a5019
Johannes Rummel <johannesrummel@googlemail.com>
2014-06-27 10:58:14 +0200

    Extended testcase to demo modifying view Changed folder structure

--- tst/core/res/1-binding/test.js
+++
@@ -1,7 +0,0 @@
-function test(model, $) {
-    return describe("BindingJS Direction and Connector Test", function() {
-        it("should one-way bind a model attribute to the view") {
-            expect(model.m2vText).to.be.equal($(#m2v).text());
-        }
-    })
-}
