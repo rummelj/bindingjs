f5266623b7ef9f7644b01e67dce005499860d90e
Ralf S. Engelschall <rse@engelschall.com>
2014-07-08 14:29:11 +0200

    extend with the slot API and use the DOM types

--- src/core/binding.d.ts
+++ src/core/binding.d.ts
@@ -68,7 +68,45 @@ interface BindingJS_API_function {
 /*  external binding API  */
 interface BindingJS_API_binding {
     destroy             (                                 ): void
-    template            (   template: any                 ): BindingJS_API_binding
-    model               (   model: any                    ): BindingJS_API_binding
+    template            (   selector: string              ): BindingJS_API_binding
+    template            (   element: HTMLElement          ): BindingJS_API_binding
+    template            (   fragment: DocumentFragment    ): BindingJS_API_binding
+    model               (   model: Object                 ): BindingJS_API_binding
+    mount               (   selector: string              ): BindingJS_API_binding
+    mount               (   element: HTMLElement          ): BindingJS_API_binding
+    mount               (   cb: (
+                                fragment: DocumentFragment
+                            ) => void                     ): BindingJS_API_binding
+    slot                (   name: string                  ): BindingJS_API_slot
+}
+
+/*  external slot API  */
+interface BindingJS_API_slot {
+    length              (                                 ): number
+    find                (   element: HTMLElement          ): number
+    get                 (                                 ): any[]
+    get                 (   indexBegin: number,
+                            indexEnd: number              ): any[]
+    get                 (   index: number                 ): any
+    set                 (   index: number,
+                            elements: HTMLElement[]       ): void
+    set                 (   index: number,
+                            ...element: HTMLElement[]     ): void
+    splice              (   index: number,
+                            len: number,
+                            elements: HTMLElement[]       ): any[]
+    splice              (   index: number,
+                            len: number,
+                            ...element: HTMLElement[]     ): any[]
+    push                (   elements: HTMLElement[]       ): void
+    push                (   ...element: HTMLElement[]     ): void
+    pop                 (                                 ): any
+    unshift             (   elements: HTMLElement[]       ): void
+    unshift             (   ...element: HTMLElement[]     ): void
+    shift               (                                 ): any
+    delete              (   index: number                 ): void
+    delete              (   elements: HTMLElement[]       ): void
+    delete              (   ...element: HTMLElement[]     ): void
+    delete              (                                 ): void
 }
 
