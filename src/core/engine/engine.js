019557f5bcaf8675ac014cf1208ac8d29943ae42
Johannes Rummel <johannesrummel@googlemail.com>
2014-07-23 16:04:30 +0200

    Adapting Copyright

--- src/core/engine/engine.js
+++ src/core/engine/engine.js
@@ -1,6 +1,6 @@
 /*
 **  BindingJS -- View Data Binding for JavaScript <http://bindingjs.com>
-**  Copyright (c) 2014 Ralf S. Engelschall <http://engelschall.com>
+**  Copyright (c) 2014 Johannes Rummel
 **
 **  This Source Code Form is subject to the terms of the Mozilla Public
 **  License (MPL), version 2.0. If a copy of the MPL was not distributed
